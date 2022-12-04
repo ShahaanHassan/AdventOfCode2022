1bec6405e87f9e223c233212039d22a41803fe7e
ShahaanHassan <shahaan.hassan@gmail.com>
2022-12-04 12:57:17 +0000

    Minor refactor

--- days/day4/day4a.py
+++ days/day4/day4a.py
@@ -1,13 +1,12 @@
 def read_input() -> list:
     with open('./days/day4/input.txt', 'r') as file:
-        return [line.rstrip() for line in file]
+        return [line.rstrip().split(',') for line in file]
 
 
 def find_overlaps() -> int:
     duties = read_input()
     score = 0
-    for line in read_input():
-        first, second = line.split(',')
+    for first, second in read_input():
         a,b = first.split('-')
         c,d = second.split('-')
 
