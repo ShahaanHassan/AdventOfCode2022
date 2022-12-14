488d3408578acf677b2d7c38f0b4e0b0bf6a6d09
ShahaanHassan <shahaan.hassan@gmail.com>
2022-12-14 10:32:43 +0000

    Little better

--- days/day13/day13a.py
+++ days/day13/day13a.py
@@ -2,7 +2,7 @@ from ast import literal_eval
 
 def read_input() -> list:
     with open('./days/day13/input.txt', 'r') as file:
-        return [list(convert_to_list(line.split('\n'))) for line in file.read().split('\n\n')]
+        return [x for line in file for x in convert_to_list(line.rstrip().split()) if line != '\n']
 
 
 def convert_to_list(pair: list) -> list:
@@ -13,34 +13,35 @@ def convert_to_list(pair: list) -> list:
 def check_pairs():
     input = read_input()
     total = 0
-    for i, pair in enumerate(input, 1):
-        if right_order(pair[0], pair[1]) == 1:
-            total += i
+    for i in range(0, len(input), 2):
+        first, second = input[i], input[i+1]
+        if right_order(first, second) == 1:
+            total += (i // 2) + 1
     return total
-    
 
-def right_order(first, second):    
-    if isinstance(first, list) and isinstance(second, list):
-        for i in range(max(len(first), len(second))):
-            if len(first) < i+1:
-                return 1
-            if len(second) < i+1:
-                return 0
-            order = right_order(first[i], second[i])
-            if order is not None:
-                return order
-    
+
+def right_order(first, second) -> int: 
     if isinstance(first, int) and isinstance(second, int):
         if first < second:
             return 1
         elif first > second:
-            return 0
+            return -1
 
     if isinstance(first, int) and isinstance(second, list):
         return right_order([first], second)
 
     if isinstance(first, list) and isinstance(second, int):
         return right_order(first, [second])
+
+    if isinstance(first, list) and isinstance(second, list):
+        for i in range(max(len(first), len(second))):
+            if len(first) <= i:
+                return 1
+            if len(second) <= i:
+                return -1
+            order = right_order(first[i], second[i])
+            if order is not None:
+                return order
 
 
 print(check_pairs())
--- days/day13/day13b.py
+++ days/day13/day13b.py
@@ -3,7 +3,7 @@ from functools import cmp_to_key
 
 def read_input() -> list:
     with open('./days/day13/input.txt', 'r') as file:
-        return [list(convert_to_list(line.split('\n'))) for line in file.read().split('\n\n')]
+        return [x for line in file for x in convert_to_list(line.rstrip().split()) if line != '\n']
 
 
 def convert_to_list(pair: list) -> list:
@@ -11,20 +11,13 @@ def convert_to_list(pair: list) -> list:
         yield literal_eval(li)
 
 
-def check_pairs():
-    input = read_input()
-    packets = []
-    total = 0
-    for i, pair in enumerate(input, 1):
-        packets.append(pair[0])
-        packets.append(pair[1])
-        if right_order(pair[0], pair[1]) == 1:
-            total += i
-    a = sorted(packets + [[[2]]] + [[[6]]], key = cmp_to_key(right_order), reverse=True)
-    return (a.index([[2]]) + 1) * (a.index([[6]]) + 1)
+def check_pairs() -> int:
+    input = read_input() + [[[2]], [[6]]]
+    ordered = sorted(input, key=cmp_to_key(right_order), reverse=True)
+    return (ordered.index([[2]]) + 1) * (ordered.index([[6]]) + 1)
     
 
-def right_order(first, second):    
+def right_order(first, second) -> int: 
     if isinstance(first, int) and isinstance(second, int):
         if first < second:
             return 1
@@ -39,13 +32,14 @@ def right_order(first, second):
 
     if isinstance(first, list) and isinstance(second, list):
         for i in range(max(len(first), len(second))):
-            if len(first) < i + 1:
+            if len(first) <= i:
                 return 1
-            if len(second) < i + 1:
+            if len(second) <= i:
                 return -1
             order = right_order(first[i], second[i])
             if order is not None:
                 return order
+
 
 
 print(check_pairs())
